a4ae46f4b4bea78fe376ac0b5b844d116c83a7b9
1amBulletproof <brandon.tarney@gmail.com>
2017-12-03 21:16:17 -0500

    add morph_masker v1 working

---
+++ morph_masker.py
@@ -0,0 +1,58 @@
+#!/usr/local/bin/python2
+#@Author Brandon Tarney
+#@Since  11/30/2017
+
+#morphology  masking: use morphology after thresholding to detect text
+
+from masker import Masker
+import cv2
+import numpy as np
+import time
+
+class MorphMasker(Masker):
+    def __init__(self, name="MorphMasker"):
+        self.name = name
+
+
+    def mask_img_annotations(self, img):
+        start_time = time.time()
+        masked_img = img.copy()
+        masked_img = self.get_edge_mask(masked_img)
+        end_time = time.time()
+        Masker.performance = (end_time - start_time)
+        return masked_img
+
+
+    def get_edge_mask(self, img):
+        masked_img = img.copy()
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        #Blur the image to reduce the noise
+        blurred_gray_img = cv2.blur(gray_img, (11, 11))
+
+        #Adaptive threshold to control for different lighting but this will produce noise!
+        threshold_mask = cv2.adaptiveThreshold(blurred_gray_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                cv2.THRESH_BINARY_INV, 7, 1)
+
+        #Remove the noise
+        kernel = np.ones((3, 3), np.uint8)
+        low_noise_mask = cv2.erode(threshold_mask, kernel, iterations=2)
+
+        #Combine the text area into word-blobs so they are large
+        mask = cv2.dilate(low_noise_mask, kernel, iterations=11)
+
+        #Find the contours of the masked regions (this will get us word-blobs)
+        contour_img, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+            # compute the bounding box for the rectangle
+            (x, y, w, h) = cv2.boundingRect(contour)
+            #Check for "blobs of words" which should be of decent size (which eliminates most noise!)
+            if w >= 50 and w <= 400 and h >= 30 and h <= 85: 
+                # crop the ROI and then threshold the grayscale
+                # ROI to reveal the digit
+                masked_img[y:y+h, x:x+w] = (0,0,0)
+        return masked_img
+
+
